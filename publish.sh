7e4329b399f386bd714fc354c4e00c458a0c4492
Iben Van de Veire <ibenvandeveire@gmail.com>
2025-08-07 08:10:06 +0200

    feat(setup): Add publish script

---
+++ publish.sh
@@ -0,0 +1,39 @@
+# publish
+# Ask the user which package they want to publish
+echo "Which package would you like to publish?"
+read package
+# Ask the user which version they want to publish
+echo "What version do you want to bump?"
+read version
+# Run documentation script
+echo "Running documentation"
+npx compodoc -p tsconfig.doc.json --output docs --silent
+# Commit the new documentation
+git add --all
+git commit -s -m "Update ngx-$package documentation"
+# Navigate to the package folder (currently only works for Angular packages)
+cd libs/angular/$package
+# Bump the version
+npm version $version
+# Read the new version
+NEW_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[", ]//g')
+# Make a new commit for the new version
+git add --all
+git commit -s -m "build(ngx-$package):v$NEW_VERSION"
+# Tag the new version
+git tag -a "@ibenvandeveire/ngx-$package/$NEW_VERSION" -m "New release"
+# Push the new version
+git push --follow-tags
+# Go back to main folder
+cd ../../../
+# Build the package
+echo "Building package"
+npm run build "ngx-$package"
+# Go to dist folder
+cd dist/libs/angular/$package
+# Publish the package
+npm publish
