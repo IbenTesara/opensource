ae5932647572cb4ff3111e6b1298142a3b3d7f12
Iben Van de Veire <ibenvandeveire@gmail.com>
2026-08-21 12:09:05 +0200

    feat(utils): Update utils documentation

---
+++ publish-js.sh
@@ -0,0 +1,39 @@
+# publish
+# Ask the user which package they want to publish
+echo "Which package would you like to publish?"
+read package
+# Ask the user which version they want to publish
+echo "What version do you want to bump?"
+read version
+# Run documentation script
+echo "Running documentation"
+npx compodoc -p tsconfig.doc.json --output docs --silent
+# Commit the new documentation
+git add --all
+git commit -m "feat($package): Update $package documentation"
+# Navigate to the package folder (Only works for Javascript packages)
+cd libs/javascript/$package
+# Bump the version
+npm version $version
+# Read the new version
+NEW_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[", ]//g')
+# Make a new commit for the new version
+git add --all
+git commit -m "build($package):v$NEW_VERSION"
+# Tag the new version
+git tag -a "@ibenvandeveire/$package/$NEW_VERSION" -m "New release"
+# Push the new version
+git push --follow-tags
+# Go back to main folder
+cd ../../../
+# Build the package
+echo "Building package"
+npm run build "ngx-$package"
+# Go to dist folder
+cd dist/libs/javascript/$package
+# Publish the package
+npm publish
